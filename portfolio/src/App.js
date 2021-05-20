f42e8583d99d91534f7f3910c2c724c022805a41
Paul Pahikainen <paupah@gmail.com>
2021-05-20 01:23:47 -0700

    add initial design and modal functionality.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
+import me from './images/paul-drawing.png';
+import React, { useState } from 'react';
+import { cards } from './cards.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="flex-row">
+				<div className="header__copy-container">
+					<h1 className="header__title">paul</h1>
+					<h1 className="header__title">pahikainen</h1>
+					<h3 className="header__sub-title">is a fuckin engineer, bruv</h3>
+					<p className="copy">blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. </p>
+				</div>
+
+				<div className="header__img-container">
+					<img src={me} alt="portrait drawing"/>
+				</div>
+			</header>
+
+			<section className="top-margin">
+				<div className="about__container">
+					<h3 className="section__title">
+					about me
+					</h3>
+
+					<p className="copy">
+					blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff. blah blah blah blah stuff blah stuff and stuff.
+					</p>
+				</div>
+			</section>
+
+			<div className="split-container">
+				{ cardContents }
+			</div>
+		</div>
+	);
 }
+
+function Card(props) {
+	const [cardId, setCardId] = useState(null);
+
+	const handleShowModal = (value) => {
+		setCardId(value);
+	};
+
+	const handleHideModal = () => {
+		setCardId(null);
+	};
+
+	return (
+		<React.Fragment>
+			<div className="split-item shadow-container" onClick={() => handleShowModal(props.id)}>
+				<div className="card__image-container">
+					<img className="card__image" src={props.cardImage} alt=""/>
+				</div>
+
+				<div className="card__copy-container">
+					<h1>{props.name}</h1>
+					<p>{props.title}</p>
+					<p>{props.description}</p>
+					<p>{props.date}</p>
+				</div>
+			</div>
+
+			<Modal 
+				key={props.id}
+				show={cardId === props.id}
+				hideModal={handleHideModal}
+				name={props.name}
+				title={props.title}
+				description={props.description}
+				date={props.date}
+				img={props.img}
+			/>
+		</React.Fragment>
+	)
+
+}
+
+function Modal(props) {
+	return (
+		<React.Fragment>
+			{props.show && (
+				<div className="modal modal--open">
+					<div className="shadow-container">
+						<h1>{props.name}</h1>
+						<button onClick={props.hideModal}>close me</button>
+						<p>{props.title}</p>
+						<p>{props.description}</p>
+						<p>{props.date}</p>
+						{props.img.map((item, index) => (
+							<img className="card__image" src={item.item} key={item.item} alt=""/>
+						))}
+					</div>
+				</div>
+			)}
+		</React.Fragment>
+	)
+}
+
+let cardContents = cards.map(card => 
+	<Card { ...card } key={ card.id } />
+  )
 
 export default App;
---
+++ portfolio/src/cards.js
@@ -0,0 +1,51 @@
+export let cards = [
+	{
+        id: 1,
+		name: 'Glo',
+		title: 'Front-end engineer',
+		description: 'blah blah blah blah blah glo stuff blah blah',
+        cardImage: 'https://pbs.twimg.com/profile_images/1075137107419529216/IRLvM6Av.jpg',
+		img: [
+            {
+                item: 'https://pbs.twimg.com/profile_images/1075137107419529216/IRLvM6Av.jpg',
+            }, {
+                item: 'https://pbs.twimg.com/profile_images/1233430405845405697/3-RTZAb6.png',
+            }, {
+                item: 'https://cdn.shopify.com/s/files/1/0267/6300/1908/files/142BDEDD-8F5A-4949-BE3D-81EDB98783B1_1200x1200.jpg?v=1574184015',
+            }
+        ],
+		date: '2016-2021'
+	}, {
+        id: 2,
+		name: 'Pathcore',
+		title: 'Designer/Engineer',
+		description: 'blah blah blah blah blah pathcore stuff blah blah',
+        cardImage: 'https://pbs.twimg.com/profile_images/1233430405845405697/3-RTZAb6.png',
+		img: [
+            {
+                item: 'https://pbs.twimg.com/profile_images/1075137107419529216/IRLvM6Av.jpg',
+            }, {
+                item: 'https://pbs.twimg.com/profile_images/1233430405845405697/3-RTZAb6.png',
+            }, {
+                item: 'https://cdn.shopify.com/s/files/1/0267/6300/1908/files/142BDEDD-8F5A-4949-BE3D-81EDB98783B1_1200x1200.jpg?v=1574184015',
+            }
+        ],
+		date: '2019'
+	}, {
+        id: 3,
+		name: 'Humble Flower',
+		title: 'Front-end engineer',
+		description: 'blah blah blah blah blah humbleflower stuff blah blah',
+        cardImage: 'https://cdn.shopify.com/s/files/1/0267/6300/1908/files/142BDEDD-8F5A-4949-BE3D-81EDB98783B1_1200x1200.jpg?v=1574184015',
+		img: [
+            {
+                item: 'https://pbs.twimg.com/profile_images/1075137107419529216/IRLvM6Av.jpg',
+            }, {
+                item: 'https://pbs.twimg.com/profile_images/1233430405845405697/3-RTZAb6.png',
+            }, {
+                item: 'https://cdn.shopify.com/s/files/1/0267/6300/1908/files/142BDEDD-8F5A-4949-BE3D-81EDB98783B1_1200x1200.jpg?v=1574184015',
+            }
+        ],
+		date: '2019'
+	}
+]
